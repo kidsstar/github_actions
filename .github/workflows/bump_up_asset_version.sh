4774162306c843a4ffe83868cb6effcb63f2fdb2
Shu Masuda <shu.masuda@kidsstar.co.jp>
2022-04-25 15:49:42 +0900

    :clipboard: Support hybrid pavilions

--- .github/workflows/bump_up_asset_version.sh
+++ .github/workflows/bump_up_asset_version.sh
@@ -7,7 +7,7 @@ update_addressables() {
     return
   fi
 
-  if [[ -f "Assets/AddressableAssetsData/AddressableAssetSettings.asset" ]]; then
+  if [ -f "Assets/AddressableAssetsData/AddressableAssetSettings.asset" ]; then
     file_name="Assets/AddressableAssetsData/AddressableAssetSettings.asset"
     current_version=$(< $file_name grep "m_overridePlayerVersion" | awk '{print $2}')
     next_version=$(printf "%0${#current_version}d" $((current_version + 1)))
@@ -16,8 +16,17 @@ update_addressables() {
     echo -e "* \`$file_name\`: $current_version => $next_version" >> message
   fi
 
-  if [[ -f "Assets/Settings/PavilionSetting.asset" ]]; then
+  if [ -f "Assets/Settings/PavilionSetting.asset" ]; then
     file_name="Assets/Settings/PavilionSetting.asset"
+    current_version=$(< $file_name grep "addressablesCatalogVersion" | awk '{print $2}')
+    next_version=$((current_version + 1))
+    sed -i "s/addressablesCatalogVersion: $current_version/addressablesCatalogVersion: $next_version/" "$file_name"
+
+    echo -e "* \`$file_name\`: $current_version => $next_version" >> message
+  fi
+
+  if [ -f "Assets/Booth/Settings/PavilionSetting.asset" ]; then
+    file_name="Assets/Booth/Settings/PavilionSetting.asset"
     current_version=$(< $file_name grep "addressablesCatalogVersion" | awk '{print $2}')
     next_version=$((current_version + 1))
     sed -i "s/addressablesCatalogVersion: $current_version/addressablesCatalogVersion: $next_version/" "$file_name"
@@ -31,7 +40,7 @@ update_asset_bundle() {
     return
   fi
 
-  if [[ ! -f "Assets/Resources/Entities/ProjectContextEntity.asset" ]]; then
+  if [ ! -f "Assets/Resources/Entities/ProjectContextEntity.asset" ]; then
     return
   fi
 
